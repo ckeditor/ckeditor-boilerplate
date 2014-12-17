827c1a2cadfca13a9580c313c89a0408ea9ebe1f
Piotrek Koszuliński <pkoszulinski@gmail.com>
2014-12-17 15:37:45 +0100

    The target should default to 'git', not all targets.

--- dev/tasks/lint.js
+++ dev/tasks/lint.js
@@ -5,7 +5,7 @@
 var tools = require( './utils/tools' );
 
 module.exports = function( grunt ) {
-	grunt.registerMultiTask( 'lint', function() {
-		grunt.task.run( this.data );
+	grunt.registerTask( 'lint', function( target ) {
+		grunt.task.run( grunt.config.get( this.name )[ target || 'git' ] );
 	} );
 };
